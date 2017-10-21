74e43c1585ab7dd4680c0b2af3e98feee2fa3b38
gerolvr <gerolvr@gmx.com>
2017-10-21 14:39:34 +1100

    Commit Initial Spring Starter Project

---
+++ src/main/java/com/gerolivo/stargazerdiary/StargazerDiaryApplication.java
@@ -0,0 +1,12 @@
+package com.gerolivo.stargazerdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StargazerDiaryApplication {
+ 
+	public static void main(String[] args) {
+		SpringApplication.run(StargazerDiaryApplication.class, args);
+	}
+}
