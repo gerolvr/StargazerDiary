8fde30fa0976520925cc799e2bd059dd85a79dbb
gerolvr <gerolvr@gmx.com>
2017-10-21 18:26:32 +1100

    Add navbar in thymeleaf main layout

--- src/main/java/com/gerolivo/stargazerdiary/config/SecurityConfig.java
+++ src/main/java/com/gerolivo/stargazerdiary/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/h2-console/**").permitAll()
+                .antMatchers("/", "/h2-console/**", "/css/**", "/js/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
--- src/main/java/com/gerolivo/stargazerdiary/controllers/HomeController.java
+++ src/main/java/com/gerolivo/stargazerdiary/controllers/HomeController.java
@@ -1,17 +1,27 @@
 package com.gerolivo.stargazerdiary.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
+	@ModelAttribute
+	public void addAttributes(Model model) {
+	    model.addAttribute("showingHome", true);
+	    model.addAttribute("showingTelescopeList", false);
+	    model.addAttribute("showingMyObservationsList", false);
+	    model.addAttribute("showingAstroData", false);
+	}
+	
+	@GetMapping("/")
 	public String home() {
 		return "home";
 	}
 	
-	@RequestMapping("/login")
+	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
